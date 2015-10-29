54ba92fd00bdc2d7e57a512d4bfbefc020f1c952
Chai Wei Jun <chaiweijun@Chais-Mac.local>
2015-10-29 15:12:11 +0800

    implemented users model & controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+require 'byebug'
+
+enable :sessions
+
+get '/' do
+	erb :"static/hello"
+
+end
+
+post '/signup' do
+	@user = User.new(params[:user])
+	if @user.save
+		@message = "Thank you for signing up! You may now login."
+	else
+		@user.error
+
+  	erb :"static/index"
+  end
+end
+
+post '/login' do
+	@user = User.authenticate(params[:user][:email], params[:user][:password])
+	session[:user_id] = @user.id
+	@message = "Welcome #{@user.full_name}! Thank you for logging in."
+	redirect to 'users/:id'
+end
+
+post '/logout' do
+	session[:user_id] = nil
+	redirect to '/'
+end
+
+get '/users/:id' do
+	
+	@user = User.find_by_id(params[:id])
+
+	if @user == nil
+		@message = "User not found"
+	else
+		@message = "Name: #{@user.full_name}"
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+	# This is Sinatra! Remember to create a migration!
+	validates :full_name, :email, :password, presence: true
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+	validates :email, :uniqueness => {:case_sensitive => false}
+	validates :password, length: { in: 6..20 }
+
+	def self.authenticate(email,password)
+		user = User.where(email: email).find_by(password: password)
+		return user
+	end
+end
